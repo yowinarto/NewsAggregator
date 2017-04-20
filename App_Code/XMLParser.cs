fb3918007c929a6ad958023f12cc04ce81902e74
Winarto <yowinarto@gmail.com>
2017-04-20 22:58:29 +0700

    Added Xml Parser

---
+++ App_Code/XMLParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Diagnostics;
+using System.Text;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class XMLParser
+{
+    private List<List<string>> titles;
+    private List<List<string>> links;
+
+    public XMLParser()
+    {
+        titles = new List<List<string>>();
+        links = new List<List<string>>();
+        Parse("http://rss.detik.com/index.php/detikcom");
+        Parse("http://tempo.co/rss/terkini");
+        Parse("http://rss.vivanews.com/get/all");
+        Parse("http://www.antaranews.com/rss/terkini");
+    }
+
+    public List<List<string>> getTitles() {
+        return titles;
+    }
+
+    public List<List<string>> getLinks() {
+        return links;
+    }
+
+    private void Parse(String feedLink)
+    {
+        XmlDocument feedXmlDoc = new XmlDocument();
+        feedXmlDoc.Load(feedLink);
+        XmlNodeList feedNodes = feedXmlDoc.SelectNodes("rss/channel/item");
+        StringBuilder feedContent = new StringBuilder();
+        List<string> title = new List<string>();
+        List<string> link = new List<string>();
+
+        foreach (XmlNode feedNode in feedNodes) {
+            XmlNode subNode = feedNode.SelectSingleNode("title");
+            title.Add(subNode.InnerText);
+            subNode = feedNode.SelectSingleNode("link");
+            link.Add(subNode.InnerText);
+        }
+        titles.Add(title);
+        links.Add(link);
+    }
+}
---
+++ Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label runat="server" ID="HelloWorldLabel"></asp:Label>
+        <br /><br />
+        <asp:TextBox runat="server" ID="TextInput" Height="50px" Width="181px" />
+    </div>
+        <p>
+        <asp:Button runat="server" ID="GreetButton" Text="Say Hello!" OnClick="GreetButton_Click" />
+        </p>
+    </form>
+</body>
+</html>
